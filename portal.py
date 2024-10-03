650d7971810cb7f84c9bc66eaf49fdcd5f060ba3
rozzclub <yigitgulec543@gmail.com>
2024-10-04 01:04:51 +0300

    Update and rename .gitignore to portal.py

---
+++ portal.py
@@ -0,0 +1,50 @@
+gvass 
+
+item ("awm") 
+
+"wall hack" (5) () ("color yellow")
+
+("headshotdata>headshot999")
+
+("headshot999") ("headshot999")
+("headshot999")   ("aimbothead999")
+("headshot999")    ("aimbothead999")
+                                  ("aimbothead999")
+                                    
+                                    ("aimbothead999")
+                                    ("aimbothead999")
+                                    ("aimbothead999")
+("headshot999")
+("headshot999")
+
+("headshot999")
+("headshot999")
+("headshot999")
+
+
+("hs>headshot/aimbothead>999")
+
+Gyroscope 1ArtQuality 1CameraLensSensibility 4FireCameraLensSensibility 4LimitBandage 7LimitMedical 1LimitEnergyDrinks 2LimitShouliudan 2LimitYanwudan 2LimitRanshaodan 1FPSLevel 6isFirstOpenMicCheck 3LimitFirstAidKit 2LimitSniper2X 0LimitSniper6X 0SidewaysMode 2OpenMirrorMode 2ScoreTips_TModeCnt 2BattleFPS 6BattleRenderStyle 2BattleRenderQuality 1LobbyFPS 6LobbyRenderStyle 2LobbyRenderQuality 1JoystickSprintSensitity 100LastSaveSettingBasicTM 1689938381OldMarkStyle 2SoundVisualizationType 3preTeamUpChat 1ManorRenderQuality 1CamLensSenNoneSniper 1.2000000476837158CamLensSenRedDotSniper 0.6000000238418579CamLensSen2XSniper 0.36000001430511475CamLensSen4XSniper 0.17000000178813934CamLensSen8XSniper 0.11999999731779099FireCamLensSenNoneSniper 1.2000000476837158FireCamLensSenRedDotSniper 0.6000000238418579FireCamLensSen2XSniper 0.36000001430511475FireCamLensSen4XSniper 0.17000000178813934FireCamLensSen8XSniper 0.11999999731779099GyroscopeSenNoneSniper 2.004244327545166GyroscopeSenRedDotSniper 1.0800000429153442GyroscopeSen2XSniper 0.8999999761581421GyroscopeSen4XSniper 0.6000000238418579GyroscopeSen8XSniper 0.41999998688697815MainVolumValue 1VehicleEye 1.2000000476837158ParachuteEye 1.2000000476837158CamFpFreeEye 0.8399999737739563CamLensSenNoneSniperFP 1.0399999618530273FireCamLensSenNoneSniperFP 1.0399999618530273GyroscopeSenNoneSniperFP 1.1399999856948853CamLensSen3XSniper 0.27000001072883606CamLensSen6XSniper 0.14000000059604645FireCamLensSen3XSniper 0.27000001072883606FireCamLensSen6XSniper 0.14000000059604645GyroscopeSen3XSniper 0.75GyroscopeSen6XSniper 0.5FireGyroscopeSenNoneSniper 2.004244327545166FireGyroscopeSenRedDotSniper 1.0800000429153442FireGyroscopeSen2XSniper 0.8999999761581421FireGyroscopeSen4XSniper 0.6000000238418579FireGyroscopeSen8XSniper 0.41999998688697815FireGyroscopeSen3XSniper 0.75FireGyroscopeSen6XSniper 0.5FireGyroscopeSenNoneSniperFp 1.1399999856948853FireGyroscopeSenShoulderTPP 1.2000000476837158FireGyroscopeSenShoulderFPP 1.2000000476837158CamLensSenNoneSniper_0 0.8094525337219238CamLensSenRedDotSniper_0 0.4000001549720764CamLensSen2XSniper_0 0.3000001013278961CamLensSen4XSniper_0 0.23000001907348633CamLensSen8XSniper_0 0.15000000596046448FireCamLensSenNoneSniper_0 0.6009276509284973FireCamLensSenRedDotSniper_0 0.5021058917045593FireCamLensSen2XSniper_0 0.3817066550254822GyroscopeSenNoneSniper_0 2.004244327545166GyroscopeSenRedDotSniper_0 2.0061535835266113GyroscopeSen2XSniper_0 1.707791805267334GyroscopeSen4XSniper_0 1.0075074434280396GyroscopeSen8XSniper_0 0.29966074228286743VehicleEye_0 1.399999976158142ParachuteEye_0 1.399999976158142CamFpFreeEye_0 1.2000000476837158CamLensSenNoneSniperFP_0 1.001855492591858FireCamLensSenNoneSniperFP_0 1.001855492591858GyroscopeSenNoneSniperFP_0 2.0083208084106445CamLensSen3XSniper_0 0.20313851535320282CamLensSen6XSniper_0 0.20000003278255463FireCamLensSen3XSniper_0 0.20313851535320282FireCamLensSen6XSniper_0 0.20000003278255463GyroscopeSen3XSniper_0 1.5061306953430176GyroscopeSen6XSniper_0 0.40662306547164917FireGyroscopeSenNoneSniper_0 2.0032026767730713FireGyroscopeSenRedDotSniper_0 2.0061535835266113FireGyroscopeSen2XSniper_0 1.707791805267334FireGyroscopeSen4XSniper_0 1.0075074434280396FireGyroscopeSen8XSniper_0 0.29966074228286743FireGyroscopeSen3XSniper_0 1.5061306953430176FireGyroscopeSen6XSniper_0 0.40662306547164917FireGyroscopeSenNoneSniperFp_0 2.0083208084106445FireGyroscopeSenShoulderTPP_0 1.2000000476837158FireGyroscopeSenShoulderFPP_0 1.2000000476837158
+
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
+#CONFİG İ YAPAN YİGİT CALANIN AMK 
